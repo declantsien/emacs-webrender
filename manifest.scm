892bae23d5115ffe78ab6c94aa95e02623e1cecb
Declan Tsien <declantsien@riseup.net>
2022-11-13 10:57:00 +0800

    guix shell switch to manifest

---
+++ manifest.scm
@@ -0,0 +1,25 @@
+(concatenate-manifests
+ (list (specifications->manifest
+        (list "clang-toolchain"
+	      "direnv"
+	      "git"
+	      "git:send-email"
+	      "git-cal"
+	      "gnupg"
+
+	      "emacs-rustic"
+
+	      "rust@nightly-2022-10-24:rust-docs"
+	      "rust@nightly-2022-10-24:rust-docs-json-preview"
+	      "rust@nightly-2022-10-24:clippy-preview"
+	      "rust@nightly-2022-10-24:rust-analyzer-preview"
+	      "rust@nightly-2022-10-24:rust-demangler-preview"
+	      "rust@nightly-2022-10-24:rustfmt-preview"
+	      "clang-toolchain"
+	      ;; "gcc-toolchain"
+	      "gdb"
+	      "valgrind"
+	      "strace"
+	      "glibc:debug"))
+       (package->development-manifest
+        (specification->package "emacs-next-wr"))))
