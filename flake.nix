dbae52ed3459776d316198c6a25993f425027153
Declan Tsien <declantsien@riseup.net>
2022-12-09 15:34:42 +0800

    add nix tools

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            exa
+            fd
+            autoconf
+            rust-bin.nightly."2022-10-24".default
+          ];
+
+          shellHook = ''
+            alias ls=exa
+            alias find=fd
+          '';
+        };
+      }
+    );
+}
