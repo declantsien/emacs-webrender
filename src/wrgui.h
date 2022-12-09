fangyuanziti/remacs

webrender: render basic text

src/wrgui.h
@@ -44,7 +44,7 @@ typedef int Cursor;
 typedef int RGB_PIXEL_COLOR;
 
 #define No_Cursor (0)
-#define XChar2b wchar_t
+#define XChar2b uint16_t
 
 /* Windows equivalent of XImage.  */
 typedef struct _WRImage

declantsien/emacs-webrender

src/wrgui.h
@@ -45,7 +45,7 @@ typedef int RGB_PIXEL_COLOR;
 
 typedef void *Emacs_Pixmap;
 
-#define XChar2b wchar_t
+#define XChar2b uint32_t
 
 /* Windows equivalent of XImage.  */
 typedef struct _WRImage