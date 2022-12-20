872eac11629beb84d84fe852b54ce0ca7b3bb25c
Declan Tsien <declantsien@riseup.net>
2022-12-20 23:51:18 +0800

    rm unnecessary wrapper

--- src/wr/src/select/thread.rs
+++ src/wr/src/select/thread.rs
@@ -1,12 +1,11 @@
 use std::ptr;
 
-use futures::Future;
 use libc::pselect;
 use lisp_types::bindings::thread_select;
 
 use crate::event_loop::{FdSet, Timespec};
 
-async fn async_thread_select(
+pub async fn tokio_select_fds(
     nfds: i32,
     readfds: &FdSet,
     writefds: &FdSet,
@@ -22,17 +21,5 @@ async fn async_thread_select(
             timeout.0,
             ptr::null_mut(),
         )
-    }
-}
-
-pub fn tokio_select_fds<'a>(
-    nfds: i32,
-    readfds: &'a FdSet,
-    writefds: &'a FdSet,
-    timeout: &'a Timespec,
-) -> impl Future<Output = i32> + 'a {
-    async move {
-        let nfds: i32 = async_thread_select(nfds, readfds, writefds, timeout).await;
-        nfds
     }
 }
